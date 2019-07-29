84b2bae1907b33f0b738ecdff89ed7ece0bb466c
Martin Baulig <mabaul@microsoft.com>
2019-07-29 17:04:43 -0400

    Add .NET Core app.

---
+++ DotNetTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetTest
+{
+	class Program
+	{
+		public static Task Main ()
+		{
+			return TestHttpClient ();
+		}
+
+		static Task TestHttpClient ()
+		{
+			var client = new HttpClient ();
+			return client.GetAsync ("http://broken-localhost:8888/");
+		}
+
+	}
+}
